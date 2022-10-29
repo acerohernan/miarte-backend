a6afd2d9201e0c42bceeebca75f778aeeade77c4
acerohernan <contacto.acero.hernan@gmail.com>
2022-10-28 21:36:02 -0500

    ci: other test for env variables

--- src/app/commands/runCheckEnvironmentVariables.ts
+++ src/app/commands/runCheckEnvironmentVariables.ts
@@ -3,8 +3,8 @@ import config from "../../Context/Shared/infrastructure/config";
 dotenv.config();
 
 function run() {
-  console.log(process.env.TYPEORM_USERNAME);
-  console.log(process.env.TYPEORM_PASSWORD);
+  console.log(process.env.TYPEORM_HOST);
+  console.log(process.env.TYPEORM_DATABASE);
   console.log(process.env.NODE_ENV);
   console.log(config);
 }
