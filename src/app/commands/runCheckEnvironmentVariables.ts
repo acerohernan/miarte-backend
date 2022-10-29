31fe5751b7825c163bfe239106da3bd6b8aadcdb
acerohernan <contacto.acero.hernan@gmail.com>
2022-10-28 21:32:43 -0500

    ci: checking the env variables part 2

--- src/app/commands/runCheckEnvironmentVariables.ts
+++ src/app/commands/runCheckEnvironmentVariables.ts
@@ -1,10 +1,12 @@
 import dotenv from "dotenv";
+import config from "../../Context/Shared/infrastructure/config";
 dotenv.config();
 
 function run() {
   console.log(process.env.TYPEORM_USERNAME);
   console.log(process.env.TYPEORM_PASSWORD);
   console.log(process.env.NODE_ENV);
+  console.log(config);
 }
 
 run();
